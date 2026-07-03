6ffd8f6557cb88fb91d39ae5caacbdf4933a20d0
nayeam <naeemkhann2016@gmail.com>
2026-07-03 23:37:00 +0600

    add

--- src/globalComponents/footer/footerTop/footerText.jsx
+++ src/globalComponents/footer/footerTop/footerText.jsx
@@ -4,11 +4,11 @@ const FooterText = () => {
   return (
     <>
    <div className=' text-white font-poppins font-semibold text-xl leading-8'>
-     <p className='text-center font-semibold text-xl'>© 2026 FD PLASTIX, TOOLZ & SPAREZ LTD, CRN: 17190057. EORI: ??? <br />
+     <p className='text-center font-semibold text-xl'>© 2026 FD PLASTIX, TOOLZ & SPAREZ LTD, CRN: 17190057. EORI: GB048164708000 <br />
 Registered Office Address: The Storeage Team of A.R.T. Centre, Tan House Lane, Widnes, <br /> WA8 ORR, UNITED KINGDOM</p>
 
 <p  className='text-center font-semibold text-xl my-10'>FD PLASTIX, TOOLZ & SPAREZ LTD is an independent specialist reseller of genuine vehicle <br/> parts and power tool components. We are not an authorised dealer, partner, or affiliate <br /> of any vehicle or tool manufacturer. All brand names, logos, trademarks, and model <br /> designations are the property of their respective owners and are used purely for <br /> compatibility and product identification purposes. <br />
-<u>Website by FD & ?</u></p>
+<u>Website by FD & NS</u></p>
 
 <p className='text-center font-semibold text-xl'>© FD PLASTIX, TOOLZ & SPAREZ LTD 2026 | All rights reserved <br />
 FD PLASTIX, TOOLZ & SPAREZ LTD T/A FD PTns Company Number: 17190057</p>
