4fcfc3b0e4a63f4cfb60fdbd2a157dd439666a4b
nayeam <naeemkhann2016@gmail.com>
2026-07-09 00:52:40 +0600

    add

--- src/pages/getInTouch/contactPhone/index.jsx
+++ src/pages/getInTouch/contactPhone/index.jsx
@@ -9,18 +9,19 @@ const ContactPhone = () => {
         <div className="md:mt-8 mt-4 font-poppins md:text-start text-center p-1 md:text-2xl text-base md:leading-12 leading-8 font-bold">
           <h2>
             GET IN TOUCH THROUGH THESE CHANNELS: <br />
-            Contact us by Email: <br />
+            Email our team: <br />
             <a href="mailto:info@plastixely.com" className="text-blue01">
               <u>info@plastixely.com</u>
             </a>{" "}
             <br />
-            (Our team replies to all enquiries within one business day. ) <br />
-            Contact us by Telephone: <br />
-            For sales or general enquiries please contact our sales telephone
-            number: <br />
+            (Our team responds to all inquiries within one business day. ){" "}
+            <br />
+            Connect with our team via Phone: <br />
+            To reach our sales department for general or commercial inquiries,
+            please call our designated helpline: <br />
             0000000000000 <br />
-            Our Customer Service and Technical Help Phone Lines opening hours
-            are as follows: <br />
+            Please find below our official customer service and technical
+            support operating hours: <br />
           </h2>
           <div className="my-5">
             <OperatingHours />
@@ -29,13 +30,13 @@ const ContactPhone = () => {
 
         <div className=" font-poppins md:text-start text-center p-1 md:text-2xl text-base md:leading-12 leading-8 font-bold">
           <h2 className="font-poppins">
-            Contact us by WhatsApp: <br />
+            Message our team via WhatsApp: <br />
             <a href="https://wa.me/447741003192" className="text-blue01">
               <u>+447741003192</u>
             </a>{" "}
             <br />
             Contact us by Post: <br />
-            If you need to write to us our postal address is : <br />
+            If you wish to write to us, our mailing address is: <br />
             FD PLASTIX, TOOLZ & SPAREZ LTD <br />
             THE STORAGE TEAM OF A.R.T. CENTRE <br />
             TAN HOUSE LANE <br /> WIDNES <br />
